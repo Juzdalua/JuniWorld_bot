88bfbc3a910203cd951159a57b6d810858a43617
juzdalua <bearwcw@naver.com>
2025-06-16 16:00:57 +0900

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -14,7 +14,7 @@ export const getFormattedTime = (): string => {
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
 };
 
-export const getFormattedTimeByDate = (d:number): string => {
+export const getFormattedTimeByDate = (d: number): string => {
   const now = new Date(d);
 
   // 날짜와 시간 포맷팅
@@ -28,4 +28,35 @@ export const getFormattedTimeByDate = (d:number): string => {
 
   //2025-02-22 22:41:45.430733
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+};
+
+export function isValidEmail(email: string) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+export const toCamelCase = (object: any) => {
+  let newObject: any = {};
+  var keys = Object.keys(object);
+  const toCamelCase = (message: string) => {
+    message = message.toLowerCase();
+    message = message.replace(/-/gi, ' ');
+    message = message.replace(/_/gi, ' ');
+    let messages = message.split(' ').map((e, i) => {
+      if (i > 0) {
+        e = e.substring(0, 1).toUpperCase() + e.substring(1);
+      }
+      return e;
+    });
+    return messages.join('');
+  };
+  for (let i = 0; i < keys.length; i++) {
+    newObject[toCamelCase(keys[i])] = object[keys[i]];
+  }
+  return newObject;
+};
+
+export const currentDate = (): string => {
+  const date = new Date();
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 };
